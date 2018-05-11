bb5b3cd737b07146c7eaf5d5003d313ec342fcc0
knipferrc <knipferrc@gmail.com>
2018-05-10 20:50:24 -0400

    minimal setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser'
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core'
-import { ServiceWorkerModule } from '@angular/service-worker'
 
 @NgModule({
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', {
-      enabled: process.env.NODE_ENV === 'production'
-    }),
     AppRoutingModule
   ],
   declarations: [AppComponent, DashboardComponent, HomeComponent],
